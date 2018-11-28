53213aef493c0c78304c3e3b0139c7a7fc75a527
karshak4859 <karshak0609@yandex.ru>
2018-11-28 12:26:54 +0300

    create searsh user

--- www/app/Http/Controllers/AdminController.php
+++ www/app/Http/Controllers/AdminController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 use App\Admin;
 use App\Login;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
@@ -54,7 +55,24 @@ class AdminController extends Controller
         if (isset($_POST['create'])) {
             return redirect()->action('AdminController@createUser');
         }
-        return view('admin.adminPage', compact('loginOk', 'value'));
+
+        if (isset($_POST['search'])) {
+
+            $search = DB::table('logins')
+                ->leftJoin('phone_users', 'logins.id', '=', 'phone_users.user_id')
+                ->leftJoin('mail_users', 'logins.id', '=', 'mail_users.user_id')
+                ->select('logins.login', 'logins.firstName', 'logins.lastName', 'logins.numberPassport','logins.identificationNumber', 'phone_users.phone_number', 'mail_users.mail')
+                ->where('logins.login', '=', $_POST['searchUser'])
+                ->orWhere('logins.firstName', '=', $_POST['searchUser'])
+                ->orWhere('phone_users.phone_number', '=', $_POST['searchUser'])
+                ->orWhere('mail_users.mail', '=', $_POST['searchUser'])
+                ->groupBy('logins.login')
+            ->get();
+
+
+        }
+
+        return view('admin.adminPage', compact('loginOk', 'value', 'search'));
     }
 
     public function createUser(Request $request)
@@ -63,8 +81,8 @@ class AdminController extends Controller
             Login::addUser($_POST['login'],$_POST['password'],$_POST['lastName'] ,$_POST['firstName'],$_POST['middleName'],$_POST['numberPassport'],$_POST['identificationNumber'],$_POST['birthday']);
             $numberpassport = Login::where('numberPassport', '=', $_POST['numberPassport'])->first();
             dump($numberpassport);
-            Phone_user::addPhone($_POST['phone'],0 ,$numberpassport->id);
-            Mail_user::addMail($_POST['mail'],0 ,$numberpassport->id);
+            Phone_user::addPhone($_POST['phone'],1 ,$numberpassport->id);
+            Mail_user::addMail($_POST['mail'],1 ,$numberpassport->id);
         }
         return view('admin.createUser' );
     }
--- www/resources/views/admin/adminPage.blade.php
+++ www/resources/views/admin/adminPage.blade.php
@@ -1,7 +1,3 @@
-
-
-
-
 {!! Form::macro('exit1', function () {
 return '<input type="submit" value="exit" name="submit">';
 });!!}
@@ -9,6 +5,9 @@ return '<input type="submit" value="exit" name="submit">';
 return '<input type="submit" value="create" name="create">';
 });!!}
 <?php  echo $loginOk->login;?> вы успешно авторизировались
+{!! Form::macro('exit3', function () {
+return '<input type="submit" value="search" name="search">';
+});!!}
 
 
 {!! Form::open(array('action' => 'AdminController@adminPage'))!!}
@@ -16,8 +15,37 @@ return '<input type="submit" value="create" name="create">';
 {!! Form::close()!!}
 
 {!! Form::open(array('action' => 'AdminController@adminPage')) !!}
-{!! Form::exit2('Создать пользователя')  !!}
+{!! Form::exit2('create users')  !!}
+{!! Form::close() !!}
+
+{!! Form::open(array('action' => 'AdminController@adminPage')) !!}
+{!!  Form::text('searchUser')!!}
+{!! Form::exit3('search')  !!}
 {!! Form::close() !!}
 
 
+<?php if(isset($_POST['search'])){ ?>
+    <table>
+        <tr>
+            <td>login</td>
+            <td>firstName</td>
+            <td>lastName</td>
+            <td>numberPassport</td>
+            <td>identificationNumber</td>
+            <td>phone_number</td>
+            <td>mail</td>
+        </tr>
+        <?php foreach ($search as $row) {?>
+        <tr>
+            <td><?php echo $search[0]->login?></td>
+            <td><?php echo $search[0]->firstName?></td>
+            <td><?php echo $search[0]->lastName?></td>
+            <td><?php echo $search[0]->numberPassport?></td>
+            <td><?php echo $search[0]->identificationNumber?></td>
+            <td><?php echo $search[0]->phone_number?></td>
+            <td><?php echo $search[0]->mail?></td>
+        </tr>
+        <?php }?>
+    </table>
+<?php }?>
 
