f70d904346993454ef5b0f757dbbe0f9ce8947ec
karshak4859 <karshak0609@yandex.ru>
2018-12-10 15:14:27 +0300

    commit

---
+++ www/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrejkarshakevich
+ * Date: 12/10/18
+ * Time: 11:15 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class PaymentController extends Controller
+{
+
+}
---
+++ www/resources/views/payment/paymentPage.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrejkarshakevich
+ * Date: 12/10/18
+ * Time: 11:24 AM
+ */
