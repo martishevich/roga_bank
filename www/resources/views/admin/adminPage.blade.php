4f3881e4c725d9cdc1f577042a5365a279e0fee6
romandunik <wrask7@gmail.com>
2018-12-13 16:03:59 +0300

    fix create && update validation

--- www/resources/views/admin/adminPage.blade.php
+++ www/resources/views/admin/adminPage.blade.php
@@ -1,85 +1,86 @@
 @extends('layout')
+@section('content')
 
-<div class="container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php  echo $loginOk->login;?>, you logged in successfully
 
-    <div class="row">
-        <div class="col-md-12">
-            <?php  echo $loginOk->login;?>, you logged in successfully
+                {!! Form::macro('exit1', function () {
+                return '<input type="submit" value="exit" name="submit" class="btn btn-danger">';
+                });!!}
+                {!! Form::macro('exit2', function () {
+                return '<input type="submit" value="create user" name="create" class="btn btn-warning">';
+                });!!}
+                {!! Form::macro('exit3', function () {
+                return '<input type="submit" value="search" name="search" class="btn btn-primary">';
+                });!!}
 
-            {!! Form::macro('exit1', function () {
-            return '<input type="submit" value="exit" name="submit" class="btn btn-danger">';
-            });!!}
-            {!! Form::macro('exit2', function () {
-            return '<input type="submit" value="create user" name="create" class="btn btn-warning">';
-            });!!}
-            {!! Form::macro('exit3', function () {
-            return '<input type="submit" value="search" name="search" class="btn btn-primary">';
-            });!!}
+                <div class="action text-center" style="display: flex; justify-content: center;">
+                    {!! Form::open(array('action' => 'AdminController@adminPage'))!!}
+                    {!! Form::exit1()!!}
+                    {!! Form::close()!!}
 
-            <div class="action text-center" style="display: flex; justify-content: center;">
-                {!! Form::open(array('action' => 'AdminController@adminPage'))!!}
-                {!! Form::exit1()!!}
-                {!! Form::close()!!}
+                    {!! Form::open(array('action' => 'AdminController@adminPage')) !!}
+                    {!! Form::exit2('create users')  !!}
+                    {!! Form::close() !!}
+                </div>
 
                 {!! Form::open(array('action' => 'AdminController@adminPage')) !!}
-                {!! Form::exit2('create users')  !!}
+                {!! Form::text('searchUser')!!}
+                {!! Form::exit3('search')  !!}
                 {!! Form::close() !!}
             </div>
-
-            {!! Form::open(array('action' => 'AdminController@adminPage')) !!}
-            {!! Form::text('searchUser')!!}
-            {!! Form::exit3('search')  !!}
-            {!! Form::close() !!}
         </div>
-    </div>
 
 
-    <div class="row">
-        <div class="col-md-12">
-            <?php if(isset($_POST['search'])){ ?>
-            <table class="table">
-                <tr>
-                    <th scope="col">Login</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Middle Name</th>
-                    <th scope="col">Passport Number</th>
-                    <th scope="col">Identification Number</th>
-                    <th scope="col">Phone Number</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Actions</th>
-                </tr>
-                <?php foreach ($search as $row) {?>
-                <tr>
-                    <td><?php echo $search[0]->login?></td>
-                    <td><?php echo $search[0]->firstName?></td>
-                    <td><?php echo $search[0]->lastName?></td>
-                    <td><?php echo $search[0]->middleName?></td>
-                    <td><?php echo $search[0]->numberPassport?></td>
-                    <td><?php echo $search[0]->identificationNumber?></td>
-                    <td><?php echo $search[0]->phone_number?></td>
-                    <td><?php echo $search[0]->mail?></td>
-                    <td class="actions">
-                        <a href="{{ route('user.show', $search[0]->id) }}">
-                            <i class="fa fa-eye"></i>
-                        </a>
-                        <a href="{{ route('user.edit', $search[0]->id) }}">
-                            <i class="fa fa-pencil-square-o"></i>
-                        </a>
-                        {!! Form::open(['method' => 'DELETE',
-                                       'route' => ['user.delete', $search[0]->id]]) !!}
-                        <button class="deleteRecord" onclick="return confirm('Are you sure?')">
-                            <i class="fa fa-times"></i>
-                        </button>
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
+        <div class="row">
+            <div class="col-md-12">
+                <?php if(isset($_POST['search'])){ ?>
+                <table class="table">
+                    <tr>
+                        <th scope="col">Login</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Middle Name</th>
+                        <th scope="col">Passport Number</th>
+                        <th scope="col">Identification Number</th>
+                        <th scope="col">Phone Number</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    <?php foreach ($search as $row) {?>
+                    <tr>
+                        <td><?php echo $search[0]->login?></td>
+                        <td><?php echo $search[0]->firstName?></td>
+                        <td><?php echo $search[0]->lastName?></td>
+                        <td><?php echo $search[0]->middleName?></td>
+                        <td><?php echo $search[0]->numberPassport?></td>
+                        <td><?php echo $search[0]->identificationNumber?></td>
+                        <td><?php echo $search[0]->phone_number?></td>
+                        <td><?php echo $search[0]->mail?></td>
+                        <td class="actions">
+                            <a href="{{ route('user.show', $search[0]->id) }}">
+                                <i class="fa fa-eye"></i>
+                            </a>
+                            <a href="{{ route('user.edit', $search[0]->id) }}">
+                                <i class="fa fa-pencil-square-o"></i>
+                            </a>
+                            {!! Form::open(['method' => 'DELETE',
+                                           'route' => ['user.delete', $search[0]->id]]) !!}
+                            <button class="deleteRecord" onclick="return confirm('Are you sure?')">
+                                <i class="fa fa-times"></i>
+                            </button>
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                    <?php }?>
+                </table>
                 <?php }?>
-            </table>
-            <?php }?>
+            </div>
         </div>
     </div>
-</div>
+@endsection
 
 
 
--- www/resources/views/admin/createUser.blade.php
+++ www/resources/views/admin/createUser.blade.php
@@ -1,37 +1,105 @@
 @extends('layout')
+@section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="offset-md-2 col-md-4">
-            {!! Form::open(array('action' => 'AdminController@createUser', 'class' => 'needs-validation', 'novalidate' => 'novalidate')) !!}
-            {{--{!! Form::text('login', null, array('placeholder'=>'Login', 'id' => 'firstName', 'class' => 'form-control', 'required' => 'required'))!!}--}}
 
-            <div class="col-md-6 mb-3">
-                {!! Form::text('login', null, array('placeholder'=>'Login', 'id' => 'firstName', 'class' => 'form-control', 'required' => 'required'))!!}
-                <div class="invalid-feedback">
-                    Login is required
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @include('main.header')
             </div>
-
-            {!! Form::password('password', array('placeholder'=>'Password', 'id' => 'password', 'class' => 'form-control'))!!}
-            {!! Form::text('lastName', null, array('placeholder'=>'Last Name', 'class' => 'form-control'))!!}
-            {!! Form::text('firstName', null, array('placeholder'=>'First Name', 'class' => 'form-control'))!!}
-            {!! Form::text('middleName', null, array('placeholder'=>'Middle Name', 'class' => 'form-control'))!!}
-            {!! Form::text('numberPassport', null, array('placeholder'=>'Passport Number', 'class' => 'form-control'))!!}
-            {!! Form::text('identificationNumber', null, array('placeholder'=>'Identification Number', 'class' => 'form-control'))!!}
-            {!! Form::text('phone', null, array('placeholder'=>'Phone Number', 'class' => 'form-control'))!!}
-            {!! Form::text('mail', null, array('placeholder'=>'Email', 'class' => 'form-control'))!!}
-            {!! Form::date('birthday', 'Birthday', ['class' => 'form-control', 'max' => '2018-12-11'])!!}
-            <div class="form-control">
-                {!! Form::label('currency', 'Select currency') !!}
-                {!! Form::select('currency', array('USD' => 'USD', 'EUR' => 'EUR', 'GBP' => 'GBP', 'BYN' => 'BYN'), 'BYN')!!}
-            </div>
-            <br>
-            {!! Form::submit('Create', ['class' => 'btn btn-primary btn-lg btn-block'])  !!}
-            {!! Form::close() !!}
         </div>
-        <div class="col-md-4">
-            @include('errors')
+        <div class="row">
+            <div class="offset-md-2 col-md-4">
+                {!! Form::open(array('action' => 'AdminController@createUser', 'class' => 'needs-validation', 'novalidate' => 'novalidate')) !!}
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Login</span>
+                    </div>
+                    {!! Form::text('login', null, $attributes = $errors->has('login') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
+                    </div>
+                    {!! Form::text('password', null, $attributes = $errors->has('password') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Last Name</span>
+                    </div>
+                    {!! Form::text('lastName', null, $attributes = $errors->has('lastName') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">First Name</span>
+                    </div>
+                    {!! Form::text('firstName', null, $attributes = $errors->has('firstName') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Middle Name</span>
+                    </div>
+                    {!! Form::text('middleName', null, array('id' => 'inputmiddleName', 'class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Passport Number</span>
+                    </div>
+                    {!! Form::text('numberPassport', null, $attributes = $errors->has('numberPassport') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Identification Number</span>
+                    </div>
+                    {!! Form::text('identificationNumber', null, $attributes = $errors->has('identificationNumber') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Phone Number</span>
+                    </div>
+                    {!! Form::text('phone', null, $attributes = $errors->has('phone') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
+                    </div>
+                    {!! Form::text('mail', null, $attributes = $errors->has('mail') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Birthday</span>
+                    </div>
+                    {!! Form::date('birthday', null, $attributes = $errors->has('birthday') ? array('class' => 'form-control alert-danger') : array('class' => 'form-control'))!!}
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Currency</span>
+                    </div>
+                    {!! Form::select('currency', array('USD' => 'USD', 'EUR' => 'EUR', 'GBP' => 'GBP', 'BYN' => 'BYN'), 'BYN')!!}
+                </div>
+
+                {{--<div class="form-label-group">--}}
+                {{--{!! Form::text('login', null, array('placeholder'=>'Login', 'id' => 'inputLogin', 'class' => 'form-control', 'autofocus' => ''))!!}--}}
+                {{--{!! Form::label('inputLogin', 'Login') !!}--}}
+                {{--</div>--}}
+
+                {!! Form::submit('Create', ['class' => 'btn btn-primary btn-lg btn-block'])  !!}
+                {!! Form::close() !!}
+            </div>
+            <div class="col-md-4">
+                @include('errors')
+            </div>
         </div>
     </div>
-</div>
+@endsection
